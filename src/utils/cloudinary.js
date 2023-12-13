1b217745f02751970253b7448c2263c85a85dda1
Prayas More <prayasbm21@gmail.com>
2023-12-13 17:21:47 +0530

    fix all register bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,8 +19,10 @@ const uploadOnCloudinary = async (localFilePath) => {
 		});
 
 		//* File has been uploaded successfully
-		console.log("File is uploaded on cloudinary ", response.url);
+		// console.log("File is uploaded on cloudinary ", response.url);
+		fs.unlinkSync(localFilePath);
 		return response;  
+
 	} catch (error) {
 		//* To remove the locally saved temporary file as the upload operation got failed
 		fs.unlinkSync(localFilePath);
